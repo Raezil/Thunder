4961dc211b9f99578a1d9ef0ec024f81c14d5658
Raezil <kamil.mazurek319@gmail.com>
2025-03-15 20:27:20 +0100

    feat: cmd moved outside pkg

--- install-thunder.sh
+++ install-thunder.sh
@@ -23,8 +23,8 @@ fi
 
 # Build the thunder-generate binary
 echo "⚙️  Building thunder-generate..."
-go build -o ./pkg/cmd/protoc-gen-rpc-impl ./pkg/cmd/protoc-gen-rpc-impl.go
-sudo mv ./pkg/cmd/protoc-gen-rpc-impl /usr/local/bin
+go build -o ./cmd/protoc-gen-rpc-impl ./cmd/protoc-gen-rpc-impl.go
+sudo mv ./cmd/protoc-gen-rpc-impl /usr/local/bin
 sudo chmod +x /usr/local/bin/protoc-gen-rpc-impl
 go build -o thunder-generate generator.go
 
